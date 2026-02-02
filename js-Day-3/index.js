71ead46aadeb6a43997dd6d60c332a8a2f05a74a
Rajvendra Singh <sseema7350@gmail.com>
2026-02-02 22:19:28 +0530

    Added Day 3 files

---
+++ js-Day-3/index.js
@@ -0,0 +1,20 @@
+const user = document.getElementsByClassName("user");
+console.log(user);
+user[0].style.color="blue";
+user[0].style.backgroundColor="lightgrey";
+user[0].style.padding="10px";  
+user[0].style.textAlign="center";
+user[0].style.fontSize="20px";
+ const para = document.getElementsByClassName("para");
+console.log(para);
+para[0].style.color="black";
+para[0].style.fontSize="18px";
+para[0].style.backgroundColor="lightgrey";
+para[0].style.padding="10px";
+
+ 
+// for(let i=0; i<para.length; i++){
+//     para[i].style.color="green";
+//     para[i].style.fontSize="35px";
+// }
+console.log(para.length);
