58b730fd40049c3ff07348bbf4a5b5f7ed7bda0f
Rajvendra Singh <sseema7350@gmail.com>
2026-02-06 21:57:11 +0530

    js-Day-5

---
+++ js-Day-5/js-Day-5.js
@@ -0,0 +1,116 @@
+// function handleclick() {
+//   const button = document.getElementById("btn");
+//   button.textContent = " mantri mtt bnn baar baar click krke ! ";
+// }
+
+// function handleclick() {
+//     document.querySelector("h1").style.color = "red";
+//   const button = document.getElementById("btn");
+//   button.textContent = " mantri mtt bnn baar baar click krke ! ";
+// }
+
+function handleclick(){
+    const subheading = document.createElement("h2").textContent = "new subheading"
+    const image = document.createElement("img")
+
+    image.setAttribute("src", http://127.0.0.1:5500/10_classroom.html  
+        
+    
+    )
+    
+    image.setAttribute("alt"," Kr mangalam");
+
+    // append both h2 and image inside content div.
+    document.getElementById("content").append(subheading);
+    document.getElementById("content").append(image);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
